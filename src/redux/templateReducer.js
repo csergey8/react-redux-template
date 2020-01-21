364d7ee2e69d0e3eda441ec86535b6dcda360ea6
csergey8 <c.sergey@protonmail.com>
2020-01-21 18:06:12 +0200

    commit

---
+++ src/redux/templateReducer.js
@@ -0,0 +1,25 @@
+const types = {
+    INIT_ACTION: 'INIT_ACTION'
+}
+
+const initialState = {
+    status: false
+}
+
+
+export const templateReducer = (state = initialState, type) => {
+    switch(type){
+      case types.INIT_ACTION:
+        return { 
+          ...state,
+          status: true
+        }
+  
+      default: 
+        return state
+    }
+  }
+  
+export const initAction = () => ({
+    type: types.INIT_ACTION
+})
